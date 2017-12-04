cebe90979998fd0fb3ae1cec459d98aa5a2c5f65
kingkk31 <kingkk31@naver.com>
2017-12-04 22:37:33 +0900

    re-accept

--- source code/2869_달팽이는 올라가고 싶다.cpp
+++ source code/2869_달팽이는 올라가고 싶다.cpp
@@ -1,25 +1,33 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <queue>
+#include <vector>
+#include <stack>
+#include <set>
+#include <map>
+#include <list>
+#include <string>
+#include <string.h>
+#include <math.h>
+#include <algorithm>
+#include <malloc.h>
+#include <functional>
+#include <time.h>
+#include <cctype>
+#include <iterator>
+#include <sstream>
+#include <fstream>
+#pragma warning(disable:4996)
 using namespace std;
+
+#define INF 987654321
 
 int main(void)
 {
-	int A,B,V;
-
-	cin>>A>>B>>V;
-
-	int i=0,cnt=0;
-
-	while(i<V)
-	{
-		i=i+A;
-
-		if(i<V)
-		{
-			i=i-B;
-		}
-		cnt++;
-	}
-
-	cout<<cnt<<endl;
-
+	int a, b, v;
+	scanf("%d %d %d", &a, &b, &v);
+	printf("%d\n", ((v-a)/(a-b)) + (((v - a) % (a - b)) == 0 ? 0 : 1) + 1);
+	
+	return 0;
 }
